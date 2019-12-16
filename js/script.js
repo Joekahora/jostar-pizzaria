401f7a895c1eee86e84f99300e87ff9703298725
JoeKahora <gachau.joel@gmail.com>
2019-12-16 10:16:17 +0300

    Added functions to the business logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-function myFunction(){
-    
-    event.preventDefault();
-}
+// business logic
+function pizzaOptions(sizes,crusts,toppings){
+    this.size = sizes;
+    this.crust = crusts;
+    this.topping =toppings
+};
+function pizzaSizes(small,medium,large){
+    this.small=small;
+    this.medium=medium;
+    this.large=large;
+};
+function pizzaCrusts(crispy,stuffed,gluttenfree){
+    this.crispy=crispy;
+    this.stuffed=stuffed;
+    this.gluttenfree=gluttenfree;
+};
+function pizzaToppings(pineapple,pepperoni,mushrooms,onion,sausages){
+    this.pineapple=pineapple;
+    this.pepperoni=pepperoni;
+    this.mushrooms=mushrooms;
+    this.onion=onion;
+    this.sausages=sausages;
+};
+// user interface
